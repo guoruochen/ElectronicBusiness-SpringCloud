59c61e2395ffd78fb855188c9d596b76583f90bb
郭若辰 <guoruochen@ycsjjqr.com>
2017-02-25 21:04:03 +0800

    搭建路由网关模块

---
+++ gateway/src/main/java/com/grc/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.grc.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author 郭若辰
+ * @create 2017-02-25 20:28
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
