dae5588b1ca4a805c9720282033bd810bfa9d2c6
郭若辰 <guoruochen@ycsjjqr.com>
2017-02-25 20:24:22 +0800

    搭建配置管理模块

---
+++ config/src/main/java/com/grc/ConfigApplication.java
@@ -0,0 +1,19 @@
+package com.grc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author 郭若辰
+ * @create 2017-02-25 19:33
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
