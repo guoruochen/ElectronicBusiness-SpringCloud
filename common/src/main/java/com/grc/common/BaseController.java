eda8b9c8a39d279030f6f7c0d1fdd2f2ccd261a6
郭若辰 <111>
2017-03-01 17:25:18 +0800

    添加common服务，并加入内部依赖

---
+++ common/src/main/java/com/grc/common/BaseController.java
@@ -0,0 +1,38 @@
+package com.grc.common;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 郭若辰
+ * @create 2017-03-01 17:21
+ */
+public class BaseController {
+    static final String RESPONSE_KEY_CODE = "code";
+    static final String RESPONSE_KEY_MESSAGE = "message";
+    static final String RESPONSE_KEY_RESULT = "result";
+
+    static final int DEFAULT_PAGE_SIZE = 10;
+
+
+    Map<String, Object> okResponse(Object o) {
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put(RESPONSE_KEY_CODE, 200);
+        jsonMap.put(RESPONSE_KEY_RESULT, o);
+        return jsonMap;
+    }
+
+    Map<String, Object> badResponse(String message) {
+        Map<String, Object> jsonMap = new HashMap<>();
+        jsonMap.put(RESPONSE_KEY_CODE, 500);
+        jsonMap.put(RESPONSE_KEY_MESSAGE, message);
+        return jsonMap;
+    }
+
+    String timeStamp() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSS"));
+    }
+
+}
