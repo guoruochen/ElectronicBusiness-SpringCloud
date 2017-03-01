2ff75efdfb24fc038481b5d957fef79f673b6fbe
郭若辰 <111>
2017-03-01 16:58:16 +0800

    User实体类

--- user-service/src/main/java/com/grc/user/domain/User.java
+++ user-service/src/main/java/com/grc/user/domain/User.java
@@ -1,8 +1,106 @@
 package com.grc.user.domain;
 
+import org.apache.ibatis.annotations.Update;
+
+import java.util.Date;
+
 /**
  * @author 郭若辰
  * @create 2017-02-28 15:13
  */
 public class User {
+
+    private Long id;//用户id
+
+    private String username;//用户名
+
+    private String password;//密码
+
+    private String phone;//用户手机号
+
+    private String email;//用户邮箱
+
+    private Date created;//创建日期
+
+    private Date updated;//最后更新时间
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        if (!super.equals(o)) return false;
+
+        User user = (User) o;
+
+        if (email != null ? !email.equals(user.email) : user.email != null) return false;
+        if (phone != null ? !phone.equals(user.phone) : user.phone != null) return false;
+        if (!username.equals(user.username)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + username.hashCode();
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        return result;
+    }
 }
