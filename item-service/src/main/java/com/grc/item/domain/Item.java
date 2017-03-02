6b272c04a0009791c7fe8b945d9f8445925e9b40
郭若辰 <111>
2017-03-02 13:14:44 +0800

    商品实体类

--- item-service/src/main/java/com/grc/item/domain/Item.java
+++ item-service/src/main/java/com/grc/item/domain/Item.java
@@ -1,8 +1,136 @@
 package com.grc.item.domain;
 
+import java.util.Date;
+
 /**
  * @author 郭若辰
  * @create 2017-03-01 16:20
  */
 public class Item {
+
+    private Long id;//商品id（商品编号）
+
+    private String title;//商品标题
+
+    private String sellPoint;//商品卖点
+
+    private Long price;//商品价格（单位：分）
+
+    private Long num;//库存数量
+
+    private String barcode;//条形码
+
+    private String image;//商品图片
+
+    private Long cid;//所属类目
+
+    private Long status;//商品状态，1-正常，2-下架，3-删除
+
+    private Date created;//创建时间
+
+    private Date updated;//更新时间
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSellPoint() {
+        return sellPoint;
+    }
+
+    public void setSellPoint(String sellPoint) {
+        this.sellPoint = sellPoint;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Long getNum() {
+        return num;
+    }
+
+    public void setNum(Long num) {
+        this.num = num;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Long getCid() {
+        return cid;
+    }
+
+    public void setCid(Long cid) {
+        this.cid = cid;
+    }
+
+    public Long getStatus() {
+        return status;
+    }
+
+    public void setStatus(Long status) {
+        this.status = status;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Date updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Item)) return false;
+        if (!super.equals(o)) return false;
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + title.hashCode();
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        return result;
+    }
 }
