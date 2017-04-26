98cc2125029e45b9217206f97594ec94613fb0b5
郭若辰 <111>
2017-04-26 09:56:55 +0800

    加个跨域

--- user-service/src/main/java/com/grc/user/UserApplication.java
+++ user-service/src/main/java/com/grc/user/UserApplication.java
@@ -3,6 +3,8 @@ package com.grc.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * @author 郭若辰
@@ -10,8 +12,18 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-public class UserApplication {
+public class UserApplication extends WebMvcConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
+    }
+
+    /**
+     * 跨域问题
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").
+                allowedMethods("DELETE", "POST", "GET", "PUT")
+                .allowedOrigins("*");
     }
 }
